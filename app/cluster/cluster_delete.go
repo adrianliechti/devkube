4d02f8afdd8deaf193c056480de5b80370f50e67
Adrian Liechti <adrian.liechti@zuehlke.com>
2022-08-20 17:57:48 +0200

    prepare flags for provider support

--- app/cluster/cluster_delete.go
+++ app/cluster/cluster_delete.go
@@ -3,8 +3,6 @@ package cluster
 import (
 	"github.com/adrianliechti/devkube/app"
 	"github.com/adrianliechti/devkube/pkg/cli"
-	"github.com/adrianliechti/devkube/pkg/docker"
-	"github.com/adrianliechti/devkube/pkg/kind"
 )
 
 func DeleteCommand() *cli.Command {
@@ -13,32 +11,15 @@ func DeleteCommand() *cli.Command {
 		Usage: "Delete cluster",
 
 		Flags: []cli.Flag{
-			app.NameFlag,
-		},
-
-		Before: func(c *cli.Context) error {
-			if _, _, err := docker.Info(c.Context); err != nil {
-				return err
-			}
-
-			if _, _, err := kind.Info(c.Context); err != nil {
-				return err
-			}
-
-			return nil
+			app.ProviderFlag,
+			app.ClusterFlag,
 		},
 
 		Action: func(c *cli.Context) error {
-			name := c.String("name")
+			provider, cluster := app.MustCluster(c)
 
-			provider := MustProvider(c.Context)
-
-			if name == "" {
-				name = MustCluster(c.Context, provider)
-			}
-
-			if ok, _ := cli.Confirm("Are you sure you want to delete cluster "+name, false); ok {
-				return provider.Delete(c.Context, name)
+			if ok, _ := cli.Confirm("Are you sure you want to delete cluster \""+cluster+"\"", false); ok {
+				return provider.Delete(c.Context, cluster)
 			}
 
 			return nil
