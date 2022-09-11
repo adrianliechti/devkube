a385fd1d28ee674a184738df6e1328de84f83d0e
Adrian Liechti <adrian@monobox.ch>
2022-09-11 20:49:44 +0200

    add some help

--- app/cluster/cluster_registry.go
+++ app/cluster/cluster_registry.go
@@ -42,6 +42,28 @@ func RegistryCommand() *cli.Command {
 
 			port = app.MustPortOrRandom(c, port)
 
+			cli.Info("Configure Docker to use this registry")
+			cli.Info("  {")
+			cli.Info("    ...")
+			cli.Info("    \"insecure-registries\": [")
+			cli.Infof("      \"localhost:%d\",", port)
+			cli.Infof("      \"host.docker.internal:%d\"", port)
+			cli.Info("    ]")
+			cli.Info("    ...")
+			cli.Info("  }")
+			cli.Info("  (see https://docs.docker.com/registry/insecure/#deploy-a-plain-http-registry)")
+			cli.Info()
+			cli.Info()
+			cli.Info("Push an image")
+			cli.Infof("  docker tag my-image localhost:%d/my-image", port)
+			cli.Infof("  docker push localhost:%d/my-image", port)
+			cli.Info()
+			cli.Info("Or using BuildKit:")
+			cli.Infof("  docker tag my-image host.docker.internal:%d/my-image", port)
+			cli.Infof("  docker push host.docker.internal:%d/my-image", port)
+			cli.Info()
+			cli.Info()
+
 			if err := kubectl.Invoke(c.Context, []string{"port-forward", "service/registry", fmt.Sprintf("%d:80", port)}, kubectl.WithKubeconfig(kubeconfig), kubectl.WithNamespace(DefaultNamespace), kubectl.WithDefaultOutput()); err != nil {
 				return err
 			}
