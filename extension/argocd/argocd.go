637fd1624768197477eb6d4b9a0f43023bb6d19f
Adrian Liechti <adrian@monobox.ch>
2024-10-04 18:09:50 +0200

    bump components

--- extension/argocd/argocd.go
+++ extension/argocd/argocd.go
@@ -14,7 +14,7 @@ const (
 	// https://artifacthub.io/packages/helm/argo/argo-cd
 	repoURL      = "https://argoproj.github.io/argo-helm"
 	chartName    = "argo-cd"
-	chartVersion = "7.5.0"
+	chartVersion = "7.6.8"
 )
 
 func Ensure(ctx context.Context, client kubernetes.Client) error {
--- extension/crossplane/crossplane.go
+++ extension/crossplane/crossplane.go
@@ -15,7 +15,7 @@ const (
 	// https://artifacthub.io/packages/helm/crossplane/crossplane
 	repoURL      = "https://charts.crossplane.io/stable"
 	chartName    = "crossplane"
-	chartVersion = "1.17.0"
+	chartVersion = "1.17.1"
 )
 
 func Ensure(ctx context.Context, client kubernetes.Client) error {
