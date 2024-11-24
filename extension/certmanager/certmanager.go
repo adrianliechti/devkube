a545e471ab8bd38285bdee9ead7061df9d3ab083
Adrian Liechti <adrian@monobox.ch>
2024-11-24 01:09:37 +0100

    bump components

--- extension/certmanager/certmanager.go
+++ extension/certmanager/certmanager.go
@@ -21,7 +21,7 @@ const (
 	// https://artifacthub.io/packages/helm/cert-manager/cert-manager
 	repoURL      = "https://charts.jetstack.io"
 	chartName    = "cert-manager"
-	chartVersion = "1.16.1"
+	chartVersion = "1.16.2"
 )
 
 func Ensure(ctx context.Context, client kubernetes.Client) error {
--- extension/monitoring/monitoring.go
+++ extension/monitoring/monitoring.go
@@ -18,7 +18,7 @@ const (
 	// https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack
 	repoURL      = "https://prometheus-community.github.io/helm-charts"
 	chartName    = "kube-prometheus-stack"
-	chartVersion = "66.2.1"
+	chartVersion = "66.2.2"
 )
 
 var (
